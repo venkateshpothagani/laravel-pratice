85f0219c2ea692d47cdf791300e660b1a694434d
Venkatesh Pothagani <venkateshpothagani@gmail.com>
2022-11-10 18:52:08 +0530

    cron job created

---
+++ app/Console/Commands/FiveMinsUpdate.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+use App\Models\Customer;
+use App\Mail\Remainder;
+
+class FiveMinsUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'five:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'send mail for every 5 mins';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $customers = Customer::all(['email']);
+
+        foreach ($customers as $customer) {
+            Mail::to(trim($customer["email"]))->send(new Remainder($customer["email"]));
+        }
+
+        $this->info('5 Mins Update has been send successfully');
+
+
+        return Command::SUCCESS;
+    }
+}
---
+++ resources/views/emails/remainder.blade.php
@@ -0,0 +1,2 @@
+<h1>Remainder email</h1>
+<h2>{{ $email }}</h2>
